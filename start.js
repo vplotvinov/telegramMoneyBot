b2c0a61990ce794ba439a8e3bdcc6449b484d8d5
Vladimir Plotvinov <vladimir.plotvinov@team.wrike.com>
2017-12-20 21:59:47 +0300

    added bitcoin cash and iota

--- start.js
+++ start.js
@@ -7,7 +7,9 @@ var rp = require('request-promise');
 var answer = {
     bitcoin: '',
     ether: '',
-    info: 'Write "e" for get Ethereum OR "b" for get Bitcoin. For get all, please write "all"',
+    bitcoin_cash: '',
+    iota: '',
+    info: 'Write "e" for get Ethereum \n "b" for get Bitcoin \n "bc" for get Bitcoin Cash \n "i" for get IOTA. For get all, please write "all"',
     other: 'Write "info" for get information',
 }
 
@@ -21,9 +23,13 @@ bot.on('message', function (msg) {
           body = JSON.parse(body);
           var dataBitcoin = body[0];
           var dataEther = body[1];
+          var dataBitcoinCash = body[2];
+          var dataIota = body[5];
 
-          answer.bitcoin = 'Bitcoin now: $' + dataBitcoin.price_usd + '\nLast Update: ' + new Date(Number(dataBitcoin.last_updated + '000'));
-          answer.ether = 'Ethereum now: $' + dataEther.price_usd + '\nLast Update: ' + new Date(Number(dataEther.last_updated + '000'));
+          answer.bitcoin = '~ Bitcoin now: $' + dataBitcoin.price_usd + '\nLast Update: ' + new Date(Number(dataBitcoin.last_updated + '000'));
+          answer.ether = '~ Ethereum now: $' + dataEther.price_usd + '\nLast Update: ' + new Date(Number(dataEther.last_updated + '000'));
+          answer.bitcoin_cash = '~ Bitcoin Cash now: $' + dataBitcoinCash.price_usd + '\n  Last Update: ' + new Date(Number(dataBitcoinCash.last_updated + '000'));
+          answer.iota = '~ IOTA now: $' + dataIota.price_usd + '\nLast Update: ' + new Date(Number(dataIota.last_updated + '000'));
           sendMessage(fromId, msg.text);
       } else {
           error = 'Get data error';
@@ -43,8 +49,14 @@ function sendMessage(fromId, fromMessageText) {
     case 'b':
       bot.sendMessage(fromId, answer.bitcoin);
       break;
+    case 'bc':
+      bot.sendMessage(fromId, answer.bitcoin_cash);
+      break;
+    case 'i':
+      bot.sendMessage(fromId, answer.iota);
+      break;
     case 'all':
-      bot.sendMessage(fromId, answer.bitcoin + '\n' + answer.ether);
+      bot.sendMessage(fromId, answer.bitcoin + '\n' + answer.ether + '\n' + answer.bitcoin_cash + '\n' + answer.iota);
       break;
     case 'info':
       bot.sendMessage(fromId, answer.info);
